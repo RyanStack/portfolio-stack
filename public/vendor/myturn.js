d1862da49e967f94e2eaa1d79c7acc7c20ae2160
Ryan Stack <ryanstack@Ryans-MacBook-Pro-2.local>
2014-10-12 13:25:20 -0700

    got rid of prelod junk

--- public/vendor/myturn.js
+++ public/vendor/myturn.js
@@ -1,5 +1,20 @@
-$("#flipbook").turn({
-    width: 600,
-    height: 380,
-    autoCenter: true
+$( document ).ready(function() {
 });
+
+  window.onload = function(){
+    init( callback )
+}
+
+function init( callback ){
+
+ callback()
+}
+
+function callback(){ 
+  $("#flipbook").turn({
+      width: 600,
+      height: 380,
+      autoCenter: true
+  })
+  $("#flipbookContainer").show();
+}
